e397c83aea04c8ae13256a00fd7e9fe677c20944
Petr Klus <petr@klus.co.uk>
2014-03-23 16:28:27 +0100

    first, copy contents lifx library for inclusion

---
+++ examples/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='lifx',
+      version='0.11',
+      description='LIFX Library for Python',
+      author='Sharp Hall',
+      author_email='sh@sauropod.org',
+      url='https://github.com/sharph/lifx-python',
+      packages=['lifx'],
+      license='AGPLv3'
+     )

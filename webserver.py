ba0b5958d77e047dc2b14fedbdfb326976625ff3
Petr Klus <petr@klus.co.uk>
2014-03-23 21:37:33 +0100

    works now!

--- webserver.py
+++ webserver.py
@@ -31,6 +31,9 @@ def all_lights_color(hue, saturation, brightness):
         saturation =    int(saturation)
         brightness =    int(brightness)
 
+        # pre-scale hue, TODO integrate into checks..
+        hue = hue / 355 * 100
+
         if hue > 100 or saturation > 100 or brightness > 100 \
           or hue < 0 or saturation < 0   or brightness < 0:
             raise Exception("Only accepting percentages between 0 and 100")
@@ -44,7 +47,9 @@ def all_lights_color(hue, saturation, brightness):
         return "OK"
         
     except Exception as e:
-        return "Error: {}".format(e)
+        msg = "Error: {}".format(e)
+        print(msg)
+        return msg
 
 
 def light_power(power_state=False):
@@ -56,12 +61,15 @@ def all_colour(hue, saturation, brightness):
 
     # hue, saturation, brightness, kelvin, fade_time)
     # [ 0x0000, 0x0000, 0xffff, 6500, 10000 ]
-    paramsTest = [ hue, saturation, brightness, 3500, 1000]
+    paramsTest = [ hue, saturation, brightness, 3500, 500]
+    print(paramsTest)
     for light in lights:        
         light.set_color(*paramsTest)      
 
 
 # TODO some try/catch to ensure the commands got executed..
+# TODO create a lock to ensure that only one instance works on the bulbs..
+# 
 
 if __name__ == "__main__":
     btl.run(server='cherrypy', host='0.0.0.0', port=8888)
